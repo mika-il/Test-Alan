9a3a239414aedaf49d00e022de6c11b28b8a7c6c
Alan <alan.ismas@pacificprime.com>
2022-06-28 15:52:24 +0700

    refactor code

--- logicTest.php
+++ logicTest.php
@@ -1,26 +1,31 @@
+<?php
+
 function transformedArray(array $inputArray) : array
 {
-    $countZero = countZero($inputArray);
-    if($countZero === 0) {
-        return $inputArray;
-    }
-
-    $restOfZero = $countZero - ceil($countZero/2);
     $countArr = count($inputArray);
     $nonZeroIndex = 0;
+    $countZero = 0;
 
     for($i=0; $i < $countArr; $i++){
         if($inputArray[$i] != 0) {
             $inputArray[$nonZeroIndex] = $inputArray[$i];
             $nonZeroIndex ++;
+        } else {
+            $countZero ++;
         }
     }
+    
+    if($countZero === 0) {
+        return $inputArray;
+    }
+    
 
     while($nonZeroIndex < $countArr) {
         $inputArray[$nonZeroIndex++] = 0;
     }
 
     $lastNonZero = 0;
+    $restOfZero = $countZero - ceil($countZero/2);
     for ($j = $countArr - 1; $j >= 0; $j--) {
         if ($inputArray[$j] === 0 && $restOfZero > 0) {
             $restOfZero--;
@@ -41,15 +46,6 @@ function transformedArray(array $inputArray) : array
     }
 
     return $inputArray;
-}
-
-function countZero(array $inputArray) : int
-{
-    $zero = array_filter($inputArray, function($item) {
-        return $item === 0;
-    });
-
-    return count($zero) ?? 0;
 }
 
 $inputArray = [3,5,6,0,7,0,1];
